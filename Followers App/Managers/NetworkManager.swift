086956ca83ac84a40b2791685cd13728266ed774
Abdallah Mahameed <a.mahameed2000@gmail.com>
2024-04-20 04:18:08 +0300

    built the URLRequestConvertible + built fetch follower function

--- Followers App/Managers/NetworkManager.swift
+++ Followers App/Managers/NetworkManager.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Alamofire
+import OSLog
 
 class NetworkManager {
     
@@ -14,6 +15,25 @@ class NetworkManager {
     
     //  To protect the class for being instantiated from outside the class.
     private init() { }
+    
+    func getFollowers(for username: String, page: Int) async throws -> [Follower]? {
+        
+        let response = await AF.request(GHRouter.followers(username: username, page: page))
+            .validate()
+            .serializingDecodable([Follower].self)
+            .response
+            
+        switch response.result {
+            
+        case .success(let followers):
+            Logger().info("followers has been fetched \\n \(followers)")
+            return followers
+            
+        case .failure( _):
+            Logger().error("\(response.debugDescription)")
+            return nil
+        }
+    }
 }
 
 enum GHRouter {
@@ -35,5 +55,17 @@ enum GHRouter {
         case .followers:
             return .get
         }
+    }
+}
+
+extension GHRouter: URLRequestConvertible {
+    func asURLRequest() throws -> URLRequest {
+        let url = baseURL.appendingPathComponent(path)
+        
+        var request = URLRequest(url: url)
+        
+        request.method = httpMethod
+        
+        return request
     }
 }
